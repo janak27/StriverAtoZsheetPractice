567da0935688ad36276835e9f8fb3f285aca787e
janak27 <80969920+janak27@users.noreply.github.com>
2022-09-11 15:37:49 +0530

    addition of code

--- src/StepOne/RecursionPractice.java
+++ src/StepOne/RecursionPractice.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class RecursionPractice {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6};
-        System.out.println(Arrays.toString(arr));
-        System.out.println(arr.length/2);
+//        int[] arr = {1,2,3,4,5,6};
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(arr.length/2);
 //        reversearr(0,arr.length-1,arr);
-        reversearr2(0,arr.length,arr);
-        System.out.println(Arrays.toString(arr));
+//        reversearr2(0,arr.length,arr);
+//        System.out.println(Arrays.toString(arr));
 
 //string palindrone
 
