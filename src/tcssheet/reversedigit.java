55e17ff69e80067975e10224a51c24cb334ff746
janak27 <80969920+janak27@users.noreply.github.com>
2022-09-18 21:01:21 +0530

    addition of code

--- src/tcssheet/reversedigit.java
+++ src/tcssheet/reversedigit.java
@@ -4,8 +4,19 @@ public class reversedigit {
     public static void main(String[] args)
     {
         int num = 4562;
+        System.out.println("the sum is "+sumof(num));
         System.out.println("Reverse of no. is "
                 + reverseDigits(num));
+    }
+
+    static int sumof(int num) {
+        int sum = 0;
+        while (num!=0){
+            sum = sum + num%10;
+            num = num/10;
+        }
+
+        return sum;
     }
 
     private static int reverseDigits(int num) {
@@ -13,8 +24,8 @@ public class reversedigit {
         int rev = 0;
         while (num > 0){
             rev = num % 10 + rev * 10;
-            System.out.println("rev is "+rev);
-            System.out.println("num is "+num);
+//            System.out.println("rev is "+rev);
+//            System.out.println("num is "+num);
             num = num / 10;
         }
         return rev;
