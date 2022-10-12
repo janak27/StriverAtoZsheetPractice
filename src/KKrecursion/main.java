e874b3140e9720482892c11ef88a3c8d4ad6ede3
janak27 <80969920+janak27@users.noreply.github.com>
2022-10-13 04:31:12 +0530

    addition of code

---
+++ src/KKrecursion/main.java
@@ -0,0 +1,17 @@
+package KKrecursion;
+
+public class main {
+    public static void main(String[] args) {
+//        printing numbers
+        int n = 0;
+    printnum(n);
+    }
+
+    static void printnum(int n) {
+        System.out.println(n);
+        if(n == 8){
+            return;
+        }
+        printnum(n+1);
+    }
+}
