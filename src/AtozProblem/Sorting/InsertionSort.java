370a57696534cf37355b4a7b53b82113cb28b065
janak27 <80969920+janak27@users.noreply.github.com>
2023-01-28 16:25:41 +0530

    🌱 its growing

--- src/AtozProblem/Sorting/InsertionSort.java
+++ src/AtozProblem/Sorting/InsertionSort.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class InsertionSort {
 
     public static void main(String[] args) {
-        int[] arr = {5,8,4,9,1};
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {9,5,1,4,3};
+//        System.out.println(Arrays.toString(arr));
         System.out.println("the sorted array" + Arrays.toString(Ins(arr)));
     }
 
     static int[] Ins(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length ; i++) {
             int curr = arr[i];
             int j = i - 1;
             while (j >= 0 && curr < arr[j]){
