bd384b88bdf8268c3c6681973e5926d93ab525ec
janak27 <80969920+janak27@users.noreply.github.com>
2023-08-18 18:57:57 +0530

    fresh recursion

--- src/KunalKushwahaDSA/Recursion/arrays/PatternAndSorts.java
+++ src/KunalKushwahaDSA/Recursion/arrays/PatternAndSorts.java
@@ -1,8 +1,33 @@
 package KunalKushwahaDSA.Recursion.arrays;
 
+import java.util.Arrays;
+
 public class PatternAndSorts {
     public static void main(String[] args) {
-        fun1(5,0);
+//        fun1(5,0);
+        int[] arr = {6,7,8,9,1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        int n = arr.length;
+        bubble(arr, arr.length - 1, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    static void bubble(int[] arr, int r, int c){
+        if (r==0){
+            return;
+        }
+        if(c < r){
+            if (arr[c] > arr[c+1]){
+                int temp = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = temp;
+            }
+            bubble(arr,r,c+1);
+        }
+        else {
+            bubble(arr,r-1,0);
+        }
     }
 
     static void fun1(int r, int c){
---
+++ src/KunalKushwahaDSA/Recursion/str/removeletter.java
@@ -0,0 +1,70 @@
+package KunalKushwahaDSA.Recursion.str;
+
+public class removeletter {
+    public static void main(String[] args) {
+        String s = skip("appleappappleappppa");
+        System.out.println(s);
+    }
+    static String skip(String test){
+        if (test.isEmpty()){
+            return "";
+        }
+
+        String x = "apple";
+        String y = "app";
+
+        if (test.startsWith(y) &&  !test.startsWith(x)){
+            return skip(test.substring(y.length()));
+        }
+        else {
+            return test.charAt(0)  + skip(test.substring(1));
+        }
+    }
+
+//    static String skip(String test){
+//        if (test.isEmpty()){
+//            return "";
+//        }
+//
+//        String x = "apple";
+//
+//        if (test.startsWith(x)){
+//            return skip(test.substring(5));
+//        }
+//        else {
+//            return test.charAt(0)  + skip(test.substring(1));
+//        }
+//    }
+
+//    static void skip(String p, String up){
+//        if (up.isEmpty()){
+//            System.out.println(p);
+//            return;
+//        }
+//
+//        char ch = up.charAt(0);
+//
+//        if (ch == 'a'){
+//            skip(p,up.substring(1));
+//        }
+//        else {
+//            skip(p+ch,up.substring(1));
+//        }
+//    }
+
+//    static String skip( String up){
+//        if (up.isEmpty()){
+//
+//            return up;
+//        }
+//
+//        char ch = up.charAt(0);
+//
+//        if (ch == 'a'){
+//             return skip(up.substring(1));
+//        }
+//        else {
+//            return  ch + skip(up.substring(1));
+//        }
+//    }
+}
